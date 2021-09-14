79cadf5a019d8b39905806c799752716ef61b6b4
Valeria Romero <valellope@gmail.com>
2021-09-13 20:00:01 -0600

    Upload files

---
+++ pizzeria.js
@@ -0,0 +1,29 @@
+var pizza = {
+    crustType: ["deep dish", "hand tossed"],
+    sauceType: ["marinara", "traditional"],
+    cheeses: ["mozarella", "feta"],
+    toppings: ["mushrooms", "olives", "onions", "pepperoni", "sausage"]
+};
+    
+
+
+function pizzaOven (crustType, sauceType, cheeses, toppings){
+    var pizza = {};
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings =  toppings;
+    return pizza;
+}
+
+var pizza1 = pizzaOven("deep dish","traditional",["mozarella"],["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+var pizza3 = pizzaOven("hand tossed", "traditional", ["feta"],["mushrooms", "pepperoni"]);
+console.log(pizza3);
+
+var pizza4 = pizzaOven("deep dish", "marinara", "mozarella", ["sausage, onions"]);
+console.log(pizza4);
